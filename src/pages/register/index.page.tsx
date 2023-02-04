47f38e7ecc7d6165b93d2a6877ef8932281ce38a
Julio <despairnatural@gmail.com>
2023-02-03 22:46:52 -0300

    Reqister

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -48,13 +48,13 @@ export default function Register() {
         name: data.name,
         username: data.username,
       });
+      await router.push("/register/connect-calendar");
     } catch (err) {
       if (err instanceof AxiosError && err?.response?.data.message) {
         alert(err.response.data.message);
-        return
+        return;
       }
       console.log(err);
-      
     }
   };
   return (
