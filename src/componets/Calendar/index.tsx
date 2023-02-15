dabc0f1913fa50c4744da62c73c49037ccb6e6ca
Julio <despairnatural@gmail.com>
2023-02-14 23:53:10 -0300

    Horarios disponiveis

--- src/componets/Calendar/index.tsx
+++ src/componets/Calendar/index.tsx
@@ -1,7 +1,7 @@
 import { getWeekDays } from "@/utils/get-weekdays";
 import dayjs from "dayjs";
 import { ArrowLeft, ArrowRight } from "phosphor-react";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import {
   CalendarActions,
   CalendarBody,
@@ -10,8 +10,21 @@ import {
   CalendarHeader,
   CalendarTiles,
 } from "./styles";
+interface CalendarWeek {
+  week: number;
+  days: Array<{
+    date: dayjs.Dayjs;
+    disabled: boolean;
+  }>;
+}
 
-export const Calendar = () => {
+type CalendarWeeks = CalendarWeek[];
+
+interface CalendarProps {
+  selectedDate: Date | null;
+  onDateSelected: (date: Date) => void;
+}
+export const Calendar = ({ onDateSelected, selectedDate }: CalendarProps) => {
   const shortWeekDays = getWeekDays({
     short: true,
   });
@@ -22,6 +35,54 @@ export const Calendar = () => {
 
   const currentMonth = currentDate.format("MMMM");
   const currentYear = currentDate.format("YYYY");
+  const calendarWeeks = useMemo(() => {
+    const daysInMonthArray = Array.from({
+      length: currentDate.daysInMonth(),
+    }).map((_, i) => currentDate.set("date", i + 1));
+
+    const fristWeekDay = currentDate.get("day");
+
+    const previousMonthFillArray = Array.from({
+      length: fristWeekDay,
+    })
+      .map((_, i) => currentDate.subtract(i + 1, "day"))
+      .reverse();
+    const lastDayInCurrentMonth = currentDate.set(
+      "date",
+      currentDate.daysInMonth()
+    );
+
+    const lastWeekDay = lastDayInCurrentMonth.get("day");
+
+    const nextMonthFillArray = Array.from({
+      length: 7 - (lastWeekDay + 1),
+    }).map((_, i) => lastDayInCurrentMonth.add(i + 1, "day"));
+
+    const calendarDays = [
+      ...previousMonthFillArray.map((date) => ({ date, disabled: true })),
+      ...daysInMonthArray.map((date) => ({
+        date,
+        disabled: date.endOf("day").isBefore(new Date()),
+      })),
+      ...nextMonthFillArray.map((date) => ({ date, disabled: true })),
+    ];
+
+    const calendarWeeks = calendarDays.reduce<CalendarWeeks>(
+      (weeks, _, i, original) => {
+        const isNewWeek = i % 7 === 0;
+
+        if (isNewWeek) {
+          weeks.push({
+            week: 1 / 7 + 1,
+            days: original.slice(i, i + 7),
+          });
+        }
+        return weeks;
+      },
+      []
+    );
+    return calendarWeeks;
+  }, [currentDate]);
 
   const handlePreviousMonth = () => {
     const previousMonthDate = currentDate.subtract(1, "month");
@@ -57,21 +118,22 @@ export const Calendar = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td>
-              <Calendarday disabled>1</Calendarday>
-            </td>
-            <td>
-              <Calendarday>2</Calendarday>
-            </td>
-            <td>
-              <Calendarday>3</Calendarday>
-            </td>
-          </tr>
+          {calendarWeeks.map(({ days, week }) => {
+            return (
+              <tr key={week}>
+                {days.map(({ date, disabled }) => (
+                  <td key={date.toString()}>
+                    <Calendarday
+                      onClick={() => onDateSelected(date.toDate())}
+                      disabled={disabled}
+                    >
+                      {date.get("date")}
+                    </Calendarday>
+                  </td>
+                ))}
+              </tr>
+            );
+          })}
         </tbody>
       </CalendarBody>
     </CalendarContainer>
--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,6 +1,8 @@
 import { Calendar } from "@/componets/Calendar";
+import { api } from "@/lib/axios";
 import dayjs from "dayjs";
-import { useState } from "react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import {
   Container,
   TimePicker,
@@ -10,15 +12,34 @@ import {
 } from "./styles";
 
 export const CalendarStep = () => {
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+  const [availability, setAvailability] = useState(null);
+  const isDateSelected = !!selectedDate;
+  const router = useRouter();
+  const username = String(router.query.username);
+  const weekDay = selectedDate ? dayjs(selectedDate).format("dddd") : null;
+  const describedDate = selectedDate
+    ? dayjs(selectedDate).format("DD[ de ]MMMM")
+    : null;
+
+  useEffect(() => {
+    if (!selectedDate) return;
+
+    api.get(`/users/${username}/availability`, {
+      params: {
+        date: dayjs(selectedDate).format("YYYY-MM-DD"),
+      },
+    });
+  }, [selectedDate]);
 
   return (
-    <Container isTimePickerOpen={false}>
-      <Calendar />
+    <Container isTimePickerOpen={isDateSelected}>
+      <Calendar selectedDate={selectedDate} onDateSelected={setSelectedDate} />
 
-      {false && (
+      {isDateSelected && (
         <TimePicker>
           <TimePickerHeader>
-            Janeiro <span>20 de Setembro</span>
+            {weekDay} <span>{describedDate}</span>
           </TimePickerHeader>
           <TimePickerList>
             <TimePickerItem>08:00h</TimePickerItem>
