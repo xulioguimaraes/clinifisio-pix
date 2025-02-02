627bbb6c9208fad443c0406d6d0e538bdb932fc1
Julio C. Guimarães <despairnatural@gmail.com>
2025-02-02 01:35:18 -0300

    ajuste para deploy

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import ReactModal from 'react-modal'
-import styles from "./styles.module.scss"
+import React from "react";
+import ReactModal from "react-modal";
+import styles from "./styles.module.scss";
 
-export const Spinner = ({isOpen}) => {
-    return (
-        <ReactModal 
-        overlayClassName={`${styles.modalOverlay}`}
-        className={`${styles.modalContent}`}
-        isOpen={isOpen}>
-
-            <div className={styles.container}></div>
-        </ReactModal>
-    )
-}
+export const Spinner = ({ isOpen }: { isOpen: boolean }) => {
+  return (
+    <ReactModal
+      overlayClassName={`${styles.modalOverlay}`}
+      className={`${styles.modalContent}`}
+      isOpen={isOpen}
+    >
+      <div className={styles.container}></div>
+    </ReactModal>
+  );
+};
